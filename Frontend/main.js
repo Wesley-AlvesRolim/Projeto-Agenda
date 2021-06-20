90b67b9323ff5eb92bf65027bc7e12d342b3f06a
Wesley-AlvesRolim <walvesrolim@gmail.com>
2021-06-20 14:44:22 -0300

    removed core-js and regenerator-runtime

--- Frontend/main.js
+++ Frontend/main.js
@@ -3,6 +3,8 @@ import 'regenerator-runtime/runtime';
 import './assets/css/global.css';
 import './assets/css/home.css';
 import './assets/css/login.css';
+import './assets/css/error.css';
+
 import { form } from './assets/js/form';
 import { formatNumber } from './assets/js/utils';
 window.onload = formatNumber();
