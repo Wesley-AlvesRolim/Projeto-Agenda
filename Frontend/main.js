28393302218929a19ebf65db190dcb7cdaf51abd
Wesley-AlvesRolim <walvesrolim@gmail.com>
2021-06-28 08:17:11 -0300

    Removing error message and loading functions

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,5 +1,3 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
 import './assets/css/global.css';
 import './assets/css/home.css';
 import './assets/css/login.css';
@@ -7,6 +5,22 @@ import './assets/css/error.css';
 
 import { form } from './assets/js/form';
 import { formatNumber } from './assets/js/utils';
-window.onload = formatNumber();
-form.validation();
-form.input();
+
+function errorMessage() {
+  const errorLayer = document.querySelector('.error-container');
+  if (errorLayer) {
+    setTimeout(() => {
+      errorLayer.classList.add('removing');
+    }, 5000);
+    setTimeout(() => {
+      errorLayer.remove();
+    }, 7000);
+  }
+}
+
+window.onload = function () {
+  formatNumber();
+  errorMessage();
+  form.validation();
+  form.input();
+};
