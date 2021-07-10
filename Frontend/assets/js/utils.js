d922c9302713241a30b0569ba3a509122571d758
Wesley-AlvesRolim <walvesrolim@gmail.com>
2021-07-10 16:51:42 -0300

    Format phone number input after load page function created

--- Frontend/assets/js/utils.js
+++ Frontend/assets/js/utils.js
@@ -10,3 +10,16 @@ export function formatNumber() {
     element.textContent = formatedValue.join('');
   });
 }
+
+export function formatPhoneNumberInInputAfterLoadPage(numberInput) {
+  if (numberInput.value.length > 1) {
+    const formatedValue = numberInput.value.split('').map((el, index) => {
+      if (index === 0) return '(' + el;
+      if (index === 1) return el + ') ';
+      if (index === 7) return '-' + el;
+      return el;
+    });
+
+    numberInput.value = formatedValue.join('');
+  }
+}
