266f649635e3ca773c03cf4562a1d9fb66e14ba4
Wesley-AlvesRolim <walvesrolim@gmail.com>
2021-07-12 12:06:01 -0300

    Creating Relationships Between Documents

--- src/models/ContactModel.js
+++ src/models/ContactModel.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose');
-
 const ContactSchema = new mongoose.Schema({
   name: { type: String, required: true },
   surname: { type: String, required: true },
   email: { type: String, required: true },
   number: { type: Number, required: true },
   createdAt: { type: Date, required: false, default: new Date() },
+  ownerId: { type: String, required: true },
 });
 
 class ContactModel {
   constructor() {
     this.ContactModel = mongoose.model('Contacts', ContactSchema);
   }
+
   async createDB(data) {
     try {
       await this.ContactModel.create({
@@ -19,24 +20,27 @@ class ContactModel {
         surname: data.surname,
         email: data.email,
         number: data.number,
+        ownerId: data.ownerId,
       });
     } catch (error) {
       return [error];
     }
   }
+
   async myData(id) {
     let data = await this.ContactModel.find();
     if (id) data = await this.ContactModel.findById(id);
     return data;
   }
+
   async editDb(id, update) {
     const data = await this.ContactModel.findByIdAndUpdate(id, update);
     return data;
   }
+
   async deleteContact(id) {
     await this.ContactModel.findByIdAndRemove(id);
     return;
   }
 }
-
 module.exports = new ContactModel();
